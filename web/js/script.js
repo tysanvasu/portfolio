59e0d8122fd65dd5a62e85869a05dc4679c9b3a7
tysanvasu <144417513+tysanvasu@users.noreply.github.com>
2023-09-25 04:15:12 -0700

    Add files via upload

---
+++ web/js/script.js
@@ -0,0 +1,9 @@
+// header
+
+
+let MenuBtn = document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click', function(e) {
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
